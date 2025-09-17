d0c8133cd953b0ec485d9cab4e975210e3d0fad3
thorn <thorn@felix.mail>
2025-09-17 11:27:12 +0200

    style: add whitespace

--- install.sh
+++ install.sh
@@ -57,6 +57,6 @@ ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 #########
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 ln -sf "$DOTFILES/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
-[! -d "$XDG_CONFIG_HOME/tmux/plugins/tpm" ] \
+[ ! -d "$XDG_CONFIG_HOME/tmux/plugins/tpm" ] \
 && git clone https://github.com/tmux-plugins/tpm \
 "$XDG_CONFIG_HOME/tmux/plugins/tpm"
