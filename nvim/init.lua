da155397fc095b988481f1ce5cbaea310f2e829e
thorn <thorn@felix.mail>
2025-10-06 13:27:50 +0200

    nvim: use spaces instead of tabs

--- nvim/init.lua
+++ nvim/init.lua
@@ -59,10 +59,11 @@ vim.o.undolevels = 10000
 vim.o.undoreload = 10000
 
 -- Tabs and indentation
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
-vim.o.expandtab = false
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.expandtab = true
 vim.o.autoindent = true
+vim.o.list = true
 
 -- No swap file
 vim.o.swapfile = false
