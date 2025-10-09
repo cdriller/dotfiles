3935719fe8891d08e9755542cb84eb74d71d19a8
thorn <thorn@felix.mail>
2025-10-09 14:39:57 +0200

    nvim: setup autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -223,6 +223,19 @@ require('lualine').setup {
 
 require("mason").setup()
 
+vim.o.completeopt = "popup,menuone,noinsert"
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+  callback = function(args)
+    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+    -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+  end,
+})
+
 vim.lsp.config('lua_ls', {
   on_init = function(client)
     if client.workspace_folders then
