1e3c8737c9b819fa81b9572302bd432db31bf43c
thorn <thorn@felix.mail>
2025-10-09 12:17:50 +0200

    nvim: install gitsigns

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,6 +7,8 @@ call plug#begin("$XDG_CONFIG_HOME/nvim/plugged")
     Plug 'junegunn/fzf.vim'
     Plug 'simnalamburt/vim-mundo'
     Plug 'tpope/vim-fugitive'
+    Plug 'lewis6991/gitsigns.nvim'
+
     Plug 'tpope/vim-unimpaired'
 
     Plug 'morhetz/gruvbox' " Theme
@@ -122,6 +124,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     vim.cmd("colorscheme gruvbox")
   end,
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+    local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = normal_bg })
+    end,
 })
 
 -- ##############
@@ -263,3 +273,72 @@ vim.lsp.config('lua_ls', {
 })
 
 vim.lsp.enable('lua_ls')
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+
+    vim.o.signcolumn = 'yes:2'
+
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+
+    map('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end)
+
+    map('n', '<leader>hS', gitsigns.stage_buffer)
+    map('n', '<leader>hR', gitsigns.reset_buffer)
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+    map('n', '<leader>hb', function()
+      gitsigns.blame_line({ full = true })
+    end)
+
+    map('n', '<leader>hd', gitsigns.diffthis)
+
+    map('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end)
+
+    map('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+    map('n', '<leader>hq', gitsigns.setqflist)
+
+    -- Toggles
+    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+    map('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', gitsigns.select_hunk)
+  end
+}
