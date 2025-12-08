24546a810495687db3e15edc1c596af223ae199c
Carlos Driller <carlosdriller16@gmail.com>
2025-12-08 09:57:33 +0100

    nvim(luasnip): add snippet for logging in c

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -6,12 +6,28 @@ return {
         local s = ls.snippet
         local t = ls.text_node
         local i = ls.insert_node
+        local fmt = require("luasnip.extras.fmt").fmt
 
         ls.add_snippets("all", {
             s("ternary", {
                 -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
                 i(1, "cond"), t(" ? "), i(2, "then"), t(" ::::: "), i(3, "else"),
             }),
+        })
+        ls.add_snippets("c", {
+            s("bspfile", fmt([[
+            #include "lib/logging.h"
+
+            #ifdef DEB_ENABLE_GLOBAL
+            static const char *pre_debstr = "{}";
+            #endif
+            #define FILE_DEB_LEVEL DEB_LEVEL_VERB
+            ]], {
+                i(1, "filename"),
+            })),
+            s("log", fmt([[
+            DPRINT_INFO(FILE_DEB_LEVEL, pre_debstr, " (%s): {}\n", __func__);
+            ]], { i(1, "message") }))
         })
     end,
 }
