4ae7450e100ad12567ba5223cd689c149e8e36fa
Carlos Driller <carlosdriller16@gmail.com>
2025-12-08 09:57:14 +0100

    nvim(harpoon): try to improve keymaps

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -4,10 +4,16 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
         { "<C-e>",       function () require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,    mode = "n", noremap = true, silent = true },
-        { "<leader>1",       function () require("harpoon"):list():select(1) end,    mode = "n", noremap = true, silent = true },
-        { "<leader>2",       function () require("harpoon"):list():select(2) end,    mode = "n", noremap = true, silent = true },
-        { "<leader>3",       function () require("harpoon"):list():select(3) end,    mode = "n", noremap = true, silent = true },
-        { "<leader>fah",       function () require("harpoon"):list():add() end,    mode = "n", noremap = true, silent = true },
+        { "<C-j>",       function () require("harpoon"):list():select(1) end,    mode = "n", noremap = true, silent = true },
+        { "<C-k>",       function () require("harpoon"):list():select(2) end,    mode = "n", noremap = true, silent = true },
+        { "<C-l>",       function () require("harpoon"):list():select(3) end,    mode = "n", noremap = true, silent = true },
+        { "<C-;>",       function () require("harpoon"):list():select(4) end,    mode = "n", noremap = true, silent = true },
+        { "<leader>fah",       function () require("harpoon"):list():add() end,    desc = "add file to harpoon", mode = "n", noremap = true, silent = true },
     },
-    config = true
+    opts = {
+        settings = {
+            save_on_toggle = true,
+            sync_on_ui_close = true,
+        },
+    }
 }
