eedb7842e26639a8f59d748a07a8429ae2f0b303
Carlos Driller <carlosdriller16@gmail.com>
2025-12-08 10:05:50 +0100

    nvim: use tabs by default
    I think it makes more sense to go with the default and change the config
    project wise when needed.

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,9 +17,6 @@ vim.o.undolevels = 10000
 vim.o.undoreload = 10000
 
 -- Tabs and indentation
-vim.o.shiftwidth = 2
-vim.o.tabstop = 2
-vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.list = true
 
