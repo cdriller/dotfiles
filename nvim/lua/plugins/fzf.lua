7a473380a2584e4982f8e70dd74d82c940b68e10
Carlos Driller <carlosdriller16@gmail.com>
2026-01-09 16:25:45 +0100

    nvim: tweek FzfLua keymaps

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -10,11 +10,12 @@ return {
     keys = {
         { "<leader>bb",       function () require("fzf-lua").buffers() end,    mode = "n", noremap = true, silent = true },
         { "<leader><leader>", function () FzfLua.global() end,                 mode = "n", noremap = true, silent = true },
-        { "<leader>ss",       function () FzfLua.live_grep() end,                   mode = "n", noremap = true, silent = true },
-        { "<leader>ff",       function () FzfLua.files({ cwd = "$HOME" }) end, mode = "n", noremap = true, silent = true },
-        { "<leader>fg",       function () FzfLua.git_status() end, mode = "n", noremap = true, silent = true },
-        { "<leader>fF",       function () FzfLua.files({ cwd = "/" }) end,     mode = "n", noremap = true, silent = true },
+        { "<leader>sg",       function () FzfLua.live_grep() end,                   mode = "n", noremap = true, silent = true },
+        { "<leader>ss",       function () FzfLua.lsp_document_symbols() end,                   mode = "n", noremap = true, silent = true },
+        { "<leader>sS",       function () FzfLua.lsp_live_workspace_symbols() end,                   mode = "n", noremap = true, silent = true },
         { "<leader>sk",       function () FzfLua.keymaps() end,     mode = "n", noremap = true, silent = true },
+        { "<leader>ff",       function () FzfLua.files({ cwd = "$HOME" }) end, mode = "n", noremap = true, silent = true },
+        { "<leader>fF",       function () FzfLua.files({ cwd = "/" }) end,     mode = "n", noremap = true, silent = true },
     },
     opts = {
         winopts = {
